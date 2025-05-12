02bfd98adb6ba25e14628855a91fa24165aa95fe
daanhoubrechts <daan.balegem@gmail.com>
2025-05-12 23:17:05 +0200

    Update team seed data to use simplified names

--- src/seeds/02_seed_teams.js
+++ src/seeds/02_seed_teams.js
@@ -1,12 +1,9 @@
 const tableName = "teams";
- 
+
 const seed = async function (knex) {
   // Deletes ALL existing entries
   await knex(tableName).truncate();
-  await knex(tableName).insert([
-    { name: "Ploeg 1",  },
-    { name: "Ploeg 2",  },
-  ]);
+  await knex(tableName).insert([{ name: "A" }, { name: "B" }, { name: "C" }]);
 };
- 
+
 export { seed };
