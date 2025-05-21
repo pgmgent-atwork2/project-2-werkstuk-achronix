a7c44505377b434b839061fdb0cfc50602d7662d
daanhoubrechts <daan.balegem@gmail.com>
2025-05-21 18:46:03 +0200

    changed: cycling order in attendance button functionality

--- public/scripts/match-attendance.js
+++ public/scripts/match-attendance.js
@@ -9,7 +9,7 @@ attendanceButtons.forEach((button) => {
     // Log the current status to help debug
     console.log("Current status:", currentStatus);
 
-    // Cycle through statuses: unknown (?) -> available (✓) -> reserve (R) -> unavailable (✗) -> unknown (?)
+    // Cycle through statuses: unknown (?) -> available (✓)  -> unavailable (✗) -> reserve (R) -> unknown (?)
     switch (currentStatus) {
       case "unknown":
         newStatus = "available";
@@ -18,21 +18,21 @@ attendanceButtons.forEach((button) => {
         button.classList.add("status-available");
         break;
       case "available":
-        newStatus = "reserve";
-        button.innerHTML = '<i class="fas fa-sync-alt"></i>';
-        button.classList.remove("status-available");
-        button.classList.add("status-reserve");
-        break;
-      case "reserve":
         newStatus = "unavailable";
         button.innerHTML = '<i class="fas fa-times"></i>';
-        button.classList.remove("status-reserve");
+        button.classList.remove("status-available");
         button.classList.add("status-unavailable");
         break;
       case "unavailable":
+        newStatus = "reserve";
+        button.innerHTML = '<i class="fas fa-sync-alt"></i>';
+        button.classList.remove("status-unavailable");
+        button.classList.add("status-reserve");
+        break;
+      case "reserve":
         newStatus = "unknown";
         button.innerHTML = '<i class="fas fa-question"></i>';
-        button.classList.remove("status-unavailable");
+        button.classList.remove("status-reserve");
         button.classList.add("status-unknown");
         break;
       default:
