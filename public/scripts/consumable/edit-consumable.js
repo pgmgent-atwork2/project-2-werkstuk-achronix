6b0444c01d8729e3b9dc09713fad7aeb2bc5fe89
Quinten <quintenclaes7@gmail.com>
2025-06-02 00:44:29 +0200

    fix: typo

--- public/scripts/consumable/edit-consumable.js
+++ public/scripts/consumable/edit-consumable.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const editConsumableHTML = `
     <div id="editConsumableModal" class="modal hidden">
       <div class="modal-content">
-        <span class="close" id="closeeditModal">
+        <span class="close" id="closeEditModal">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
           </svg>
