34677b06171438e261587ec19149275d3d3373a0
Quinten <quintenclaes7@gmail.com>
2025-06-01 20:44:11 +0200

    feat: implement renderConsumableCard function for improved consumable display

---
+++ public/scripts/consumable/consumable-card.js
@@ -0,0 +1,56 @@
+export default function renderConsumableCard(consumable, $consumablesContainer, userId) {
+  const $consumableCard = document.createElement("article");
+  $consumableCard.classList.add("consumable");
+  $consumableCard.innerHTML = `
+          <img class="consumable__image" src="${consumable.image_url}" alt="${consumable.name}">
+          <h3>${consumable.name}</h3>
+         <div class="consumable__details">
+      <div class="consumable__info">
+        <h2 class="consumable__name"> ${consumable.name}</h2>
+        <p class="consumable__price">€ ${consumable.price}</p>
+      </div>
+
+      <button
+        class="consumable__button btn btn--primary"
+        data-id=" ${consumable.id}"
+      >
+        Toevoegen
+      </button>
+      <form action="#" method="post" class="consumable__form">
+        <input
+          type="hidden"
+          name="consumable_id"
+          value=" ${consumable.id} "
+        />
+        <input
+          type="hidden"
+          name="consumable_name"
+          value=" ${consumable.name}"
+        />
+        <input
+          type="hidden"
+          name="consumable_image"
+          value="${consumable.image_url}"
+        />
+        <input type="hidden" name="user_id" value="${userId}" />
+        <input
+          type="hidden"
+          name="consumable_price"
+          value="${consumable.price}"
+        />
+        <button name="reduce" type="submit" class="reduce-count">-</button>
+        <input
+          data-consumable-id="${consumable.id}"
+          class="consumable__quantity"
+          type="number"
+          name="quantity"
+          min="0"
+          max="10"
+          value="0"
+        />
+        <button name="increase" type="submit" class="increase-count">+</button>
+      </form>
+    </div>
+        `;
+  $consumablesContainer.appendChild($consumableCard);
+}
