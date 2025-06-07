185bc0e43f5511234eb5e3874c2ef09f060000f0
Quinten <quintenclaes7@gmail.com>
2025-06-07 20:53:00 +0200

    feat: update user ID handling in dropdown and shopping cart for consistency

--- public/scripts/dropdown.js
+++ public/scripts/dropdown.js
@@ -6,13 +6,18 @@ export default function initDropdown() {
   const $loggedInUser = document.getElementById("logged-in-user");
   const $searchInputUsers = document.getElementById("search-input-users");
   const $searchUsers = document.querySelectorAll(".search-user");
+  const $userIdOrder = document.querySelectorAll(".user-id-order");
   let users = [];
 
   $searchUsers.forEach(($searchUser) => {
     users.push($searchUser.textContent);
     $searchUser.addEventListener("click", function (e) {
       const name = $searchUser.textContent;
-      $loggedInUser.value = e.target.dataset.userId;
+      $loggedInUser.value = e.target.dataset.id;
+      $userIdOrder.forEach(($userId) => {
+        $userId.value = e.target.dataset.id;
+      });
+
       if (name) {
         $button.textContent = name;
       }
@@ -34,7 +39,7 @@ export default function initDropdown() {
       const searchTerm = e.target.value.toLowerCase();
       $list.innerHTML = "";
 
-      if (searchTerm.length > 0) {
+      if (searchTerm.length > 0 || searchTerm.length === 0) {
         const filteredUsers = users.filter((user) =>
           user.toLowerCase().includes(searchTerm)
         );
@@ -42,7 +47,7 @@ export default function initDropdown() {
         $list.innerHTML = filteredUsers
           .map(
             (user) => `<li class="dropdown-item">
-            <a href="#" class="search-user" data-user-id="${user.id}">${user}</a>
+            <span class="search-user" data-user-id="${user.id}">${user}</span>
             </li>`
           )
           .join("");
