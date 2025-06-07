31efdf99463b19fd4ae0938f7f6c747f4505aa38
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-06-07 20:34:02 +0200

    Remove commented line checking for admin rights in checkAdmin middleware

--- src/middleware/checkAdmin.js
+++ src/middleware/checkAdmin.js
@@ -1,5 +1,4 @@
 export default (req, res, next) => {
-  // Check of gebruiker adminrechten heeft
   if (!req.user || !req.user.is_admin) {
     return res.status(403).render("errors/no-acces", {
       pageTitle: "Geen toegang | Ping Pong Tool",
